b6e8ff5d333d8996eaa1cab4195f4ca206859ccf
matteobjornsson <matteobjornsson@gmail.com>
2020-02-26 14:01:03 -0700

    modified pickel test

--- scratch/pickleTest.py
+++ scratch/pickleTest.py
@@ -8,11 +8,16 @@ testDictionary = {
 
 print("dictionary object: " + str(testDictionary))
 
+<<<<<<< HEAD
 with open('../files/calendar.pickle', 'wb') as test_dictionary_file:
+=======
+with open('../files/test.dictionary', 'wb') as test_dictionary_file:
+>>>>>>> ff1d7534178d26b8d9cbb85fb3ee35d652df681f
     pickle.dump(testDictionary, test_dictionary_file)
 
 print("dictionary has been pickled")
 
+<<<<<<< HEAD
 testDictionary[4] = 'BonusCalendarEvent'
 
 with open('../files/calendar.pickle', 'wb') as test_dictionary_file:
@@ -21,6 +26,9 @@ with open('../files/calendar.pickle', 'wb') as test_dictionary_file:
 print("modified dictionary has been pickled")
 
 with open('../files/calendar.pickle', 'rb') as test_dictionary_file:
+=======
+with open('../files/test.dictionary', 'rb') as test_dictionary_file:
+>>>>>>> ff1d7534178d26b8d9cbb85fb3ee35d652df681f
  
     unpickledTestDictionary = pickle.load(test_dictionary_file)
 
