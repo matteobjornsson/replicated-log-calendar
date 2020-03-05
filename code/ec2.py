909adce9b3b97d8ea4129315e32ad58e80ab1033
amylinck <amy_linck@outlook.com>
2020-03-05 15:12:28 -0700

    multithreading for socket listening

--- code/ec2.py
+++ code/ec2.py
@@ -1,4 +1,21 @@
 import socket
+import threading
+
+lock = threading.Lock()
+
+def thread_socket(c):
+   while True:
+
+      msg = c.recv(1024)
+      if not msg:
+         print("exiting socket")
+         lock.release()
+         break
+      
+      msg = msg[::-1]
+
+      c.send(msg)
+   c.close()
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
@@ -6,16 +23,20 @@ port = 8080                # Reserve a port on EC2 instance
 s.bind(('', port))        # Bind to the port
 print(host)
 
-
 s.listen(5)
-c, addr = s.accept()     # Establish connection from local
-print('Got connection from', addr)
+
 while True:
-   msg = c.recv(1024)
+   #msg = c.recv(1024)
+   c, addr = s.accept()     # Establish connection from local
+   print('Connection with', addr)
+   lock.acquire()
+   threading.Thread(target=thread_socket, args=(c,)).start()
+   """
    if msg != None:
       if msg != '':
          msg = msg.decode("utf-8")
          print(msg)
          c.send(b'kfjndkfndsk')
-
-c.close() 
+   c.close()
+   """ 
+s.close()
