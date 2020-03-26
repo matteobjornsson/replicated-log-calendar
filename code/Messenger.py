d094f95379b7f8fa78b17bea96b55cd5b3017deb
matteobjornsson <matteobjornsson@gmail.com>
2020-03-25 20:17:35 -0600

    bug from last commit

--- code/Messenger.py
+++ code/Messenger.py
@@ -119,8 +119,8 @@ class Messenger:
 				# while the connection fails, wait, and retry
 				print("Connecting to node ", destination, " at ", host_ip, port, ' ......')
 				# debug print statemet to see how in socket thread count changes
-				for s in self.in_socket_threads:
-					print(type(s))
+				for sthread in self.in_socket_threads:
+					print(type(sthread))
 				sleep(3)
 				continue
 
